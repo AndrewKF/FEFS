a75241dd487879c5e5cf628381ed8d2f886f2696
Andrew Fullerton <andrew.fullerton@gmail.com>
2025-01-07 15:49:32 -0500

    Database logging

--- index-ws.js
+++ index-ws.js
@@ -13,6 +13,16 @@ server.listen(PORT, function () {
   console.log(`Listeneing on port ${PORT}`);
 });
 
+process.on("SIGINT", () => {
+  wss.clients.forEach(function each(client) {
+    client.close();
+  });
+
+  server.close(() => {
+    shutdownDB();
+  });
+});
+
 // Begin Websockets
 
 const WebSocketServer = require("ws").Server;
@@ -29,6 +39,10 @@ wss.on("connection", function connection(ws) {
     ws.send("Welcome to my server");
   }
 
+  db.run(`INSERT INTO visitors (count, time)
+          VALUES (${numClients}, datetime('now'))
+      `);
+
   ws.on("close", function close() {
     wss.broadcast(`Current visitors: ${numClients}`);
     console.log("a client has disconnected");
@@ -40,3 +54,31 @@ wss.broadcast = function broadcast(data) {
     client.send(data);
   });
 };
+
+// End Websockets
+// Begin Database
+
+const sqlite = require("sqlite3");
+
+const db = new sqlite.Database(":memory:");
+
+db.serialize(() => {
+  db.run(`
+        CREATE TABLE visitors (
+            count INTEGER,
+            time TEXT
+        )
+        `);
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", (err, row) => {
+    console.log(row);
+  });
+}
+
+function shutdownDB() {
+  getCounts();
+  console.log("shutting down db");
+  db.close();
+}
